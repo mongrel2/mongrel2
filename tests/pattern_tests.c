df8e960cbd9e1f2ea0ed3f53af64661edb1fc273
kedrot@gmail.com <tordek>
2010-08-17 01:13:26 +0000

    Improved coverage on pattern.c

--- tests/pattern_tests.c
+++ tests/pattern_tests.c
@@ -4,15 +4,34 @@
 
 FILE *LOG_FILE = NULL;
 
-char *test_pattern_match() 
+const char *m;
+
+char *test_simple_matches()
 {
-    const char *m = pattern_match("ZED", strlen("ZED"), "ZED");
-    mu_assert(m != NULL, "Should match.");
+    m = pattern_match("ZED", strlen("ZED"), "ZED");
+    mu_assert(m != NULL, "Should match literal identically.");
+
+    m = pattern_match("ZED", strlen("ZED"), "Z.D");
+    mu_assert(m != NULL, "Should match wildcard.");
 
     m = pattern_match("ZED", strlen("ZED"), "FOO");
     mu_assert(m == NULL, "Should not match.");
 
+    return NULL;
+}
+
+char *test_simple_repetitions()
+{
     m = pattern_match("ZEEEED", strlen("ZEEEED"), "ZE*D");
+    mu_assert(m != NULL, "Should match.");
+
+    m = pattern_match("ZEEEED", strlen("ZEEEED"), "ZE*ED");
+    mu_assert(m != NULL, "Should match.");
+
+    m = pattern_match("ZED", strlen("ZEEEED"), "ZE*ED");
+    mu_assert(m != NULL, "Should match.");
+
+    m = pattern_match("ZD", strlen("ZD"), "ZE*D");
     mu_assert(m != NULL, "Should match.");
 
     m = pattern_match("ZEEEED", strlen("ZEEEED"), "ZX*D");
@@ -20,31 +39,104 @@ char *test_pattern_match()
 
     m = pattern_match("ZEEEED", strlen("ZEEEED"), "Z.*D");
     mu_assert(m != NULL, "Should match.");
-    
+
     m = pattern_match("ZXXXXD", strlen("ZXXXXD"), "ZE.*D");
     mu_assert(m == NULL, "Should not match.");
 
-    m = pattern_match("ZED", strlen("ZED"), ".*D$");
+    m = pattern_match("ZEEEED", strlen("ZEEEED"), "ZE+D");
     mu_assert(m != NULL, "Should match.");
 
-    m = pattern_match("ZED", strlen("ZED"), ".*X$");
+    m = pattern_match("ZD", strlen("ZEEEED"), "ZE+D");
     mu_assert(m == NULL, "Should not match.");
 
-    // ( and ) are ignored but allowed for routing system later
-    m = pattern_match("ZEED", strlen("ZEED"), "Z(.*D)$");
+    m = pattern_match("ZEEEED", strlen("ZEEEED"), "ZE-D");
     mu_assert(m != NULL, "Should match.");
 
+    m = pattern_match("ZD", strlen("ZEEEED"), "ZE-D");
+    mu_assert(m != NULL, "Should match.");
+
+    return NULL;
+}
+
+char *test_anchors()
+{
+    m = pattern_match("ZED", strlen("ZED"), "ZED$");
+    mu_assert(m != NULL, "Should match. Anchor at the end.");
+
+    m = pattern_match("ZED", strlen("ZED"), "ZE$");
+    mu_assert(m == NULL, "Should not match. Should have ended at $.'");
+
+    m = pattern_match("ZED", strlen("ZED"), "ED");
+    mu_assert(m == NULL, "Should not match. Implicit anchor at the start of pattern.");
+
+    return NULL;
+}
+
+char *test_blocks()
+{
+    // ( and ) are ignored but allowed for routing system later
+    m = pattern_match("ZED", strlen("ZEED"), "Z(ED)");
+    mu_assert(m != NULL, "Should match.");
+
+    return NULL;
+}
+
+char *test_frontier()
+{
     // the frontier pattern, kinda weird, could be useful
     // used to find patterns between two other patterns
     m = pattern_match("THE (QUICK) brOWN FOx JUMPS",
              strlen("THE (QUICK) brOWN FOx JUMPS"), "\\f[\\a]\\u-\\f[\\a]");
     mu_assert(m != NULL, "Should match.");
 
+    return NULL;
+}
+
+char *test_balance()
+{
     // balanced string match, matches the {...} contents
     m = pattern_match("/users/{1234}", strlen("/users/{1234}"), "/users/\\b{}");
+    mu_assert(m != NULL, "Should match balanced chars.");
+
+    m = pattern_match("/users/{1234", strlen("/users/{1234}"), "/users/\\b{}");
+    mu_assert(m == NULL, "Should not match balanced unchars.");
+
+    m = pattern_match("/users/1234}", strlen("/users/{1234}"), "/users/\\b{}");
+    mu_assert(m == NULL, "Should not match balanced unchars.");
+
+    return NULL;
+}
+
+char *test_set()
+{
+    m = pattern_match("ZED", strlen("ZED"), "Z[OE]D");
     mu_assert(m != NULL, "Should match.");
 
+    m = pattern_match("ZED", strlen("ZED"), "Z[^OE]D");
+    mu_assert(m == NULL, "Should not match.");
 
+    m = pattern_match("ZED", strlen("ZED"), "Z[A-Z]D");
+    mu_assert(m != NULL, "Should match.");
+
+    return NULL;
+}
+
+char *test_optional()
+{
+    m = pattern_match("ZED", strlen("ZED"), "ZEE?D");
+    mu_assert(m != NULL, "Should match.");
+
+    m = pattern_match("ZEED", strlen("ZEED"), "ZEE?D");
+    mu_assert(m != NULL, "Should match wildcard.");
+
+    m = pattern_match("ZEEED", strlen("ZEEED"), "ZEE?D");
+    mu_assert(m == NULL, "Should not match.");
+
+    return NULL;
+}
+
+char *test_escaped()
+{
     // char classes, which map to C functions:
     // \a : isalpha(c)
     // \c : iscntrl(c)
@@ -56,9 +148,9 @@ char *test_pattern_match()
     // \w : isalnum(c)
     // \x : isxdigit(c)
     // \z : (c == 0)  // NUL terminator
- 
+
     m = pattern_match("a \t 9 l . \n U w A \0", 19, "\\a \\c \\d \\l \\p \\s \\u \\w \\x \\z");
-    
+
     mu_assert(m != NULL, "Should match.");
 
     // escaped, non-class characters map to themselves
@@ -84,7 +176,15 @@ char *test_pattern_match()
 char * all_tests() {
     mu_suite_start();
 
-    mu_run_test(test_pattern_match);
+    mu_run_test(test_simple_matches);
+    mu_run_test(test_simple_repetitions);
+    mu_run_test(test_anchors);
+    mu_run_test(test_blocks);
+    mu_run_test(test_frontier);
+    mu_run_test(test_balance);
+    mu_run_test(test_set);
+    mu_run_test(test_optional);
+    mu_run_test(test_escaped);
 
     return NULL;
 }
