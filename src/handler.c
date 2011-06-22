89a2edffe4ca6c0f9aad4929f034841ecdab6e19
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-06-22 14:30:46 -0400

    [handler-subscribe] Don't always subscribe to the equivalent of an empty string

--- src/handler.c
+++ src/handler.c
@@ -2,23 +2,23 @@
  *
  * Copyright (c) 2010, Zed A. Shaw and Mongrel2 Project Contributors.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- * 
+ *
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- * 
+ *
  *     * Neither the name of the Mongrel2 Project, Zed A. Shaw, nor the names
  *       of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written
  *       permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -62,7 +62,7 @@ void Handler_notify_leave(Handler *handler, int id)
     void *socket = handler->send_socket;
     assert(socket && "Socket can't be NULL");
     bstring payload = NULL;
-    
+
     if(handler->protocol == HANDLER_PROTO_TNET) {
         payload = bformat("%s %d @* %s%d:%s,",
                 bdata(handler->send_ident), id,
@@ -306,7 +306,7 @@ void *Handler_recv_create(const char *recv_spec, const char *uuid)
     void *listener_socket = mqsocket(ZMQ_SUB);
     check(listener_socket, "Can't create ZMQ_SUB socket.");
 
-    int rc = zmq_setsockopt(listener_socket, ZMQ_SUBSCRIBE, uuid, 0);
+    int rc = zmq_setsockopt(listener_socket, ZMQ_SUBSCRIBE, uuid, strlen(uuid));
     check(rc == 0, "Failed to subscribe listener socket: %s", recv_spec);
     log_info("Binding listener SUB socket %s subscribed to: %s", recv_spec, uuid);
 
