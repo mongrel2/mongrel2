b71f1b9eeffd200020da4b8dcdf0da1b717c5999
Jason Miller <aidenn0@geocities.com>
2011-08-22 14:44:15 -0700

    Fix to use just bstrings

--- tools/filters/xff.c
+++ tools/filters/xff.c
@@ -7,7 +7,6 @@ extern int CLIENT_READ_RETRIES;
 StateEvent filter_transition(StateEvent state, Connection *conn, tns_value_t *config)
 {
     size_t len = 0;
-    char *data = tns_render(config, &len);
 
     int next;
     int rc = 0;
@@ -15,8 +14,6 @@ StateEvent filter_transition(StateEvent state, Connection *conn, tns_value_t *co
     int cont_len = Request_content_length(conn->req);
     int total_len = req_len+cont_len;
     bstring xff;
-    char *xffBuf;
-    unsigned sofar=0;
 
     /* This is a Hack, it appears valid though,
      * as this buffer is allocated when entering proxy
@@ -31,22 +28,17 @@ StateEvent filter_transition(StateEvent state, Connection *conn, tns_value_t *co
     rc = IOBuf_send(conn->proxy_iob, IOBuf_start(conn->iob), req_len-2);
     check(rc > 0, "Failed to send to proxy.");
 
-    xff = Request_get(conn->req, &HTTP_X_FORWARDED_FOR);
 
-    xffBuf = malloc(blength(&HTTP_X_FORWARDED_FOR)+2+blength(xff)+2);
-    memcpy(xffBuf,bdata(&HTTP_X_FORWARDED_FOR),blength(&HTTP_X_FORWARDED_FOR));
-    sofar+=blength(&HTTP_X_FORWARDED_FOR);
+    xff=bstrcpy(&HTTP_X_FORWARDED_FOR);
+    check_mem(xff)
+    rc = bcatcstr(xff,": ");
+    check(rc == BSTR_OK, "Out of memory");
+    rc = bconcat(xff,Request_get(conn->req, &HTTP_X_FORWARDED_FOR));
+    check(rc == BSTR_OK, "Out of memory");
+    rc = bcatcstr(xff,"\r\n");
+    check(rc == BSTR_OK, "Out of memory");
 
-    memcpy(xffBuf+sofar,": ",2);
-    sofar+=2;
-
-    memcpy(xffBuf+sofar,bdata(xff),blength(xff));
-    sofar+=blength(xff);
-
-    memcpy(xffBuf+sofar,"\r\n",2);
-    sofar+=2;
-
-    rc = IOBuf_send(conn->proxy_iob, xffBuf, sofar);
+    rc = IOBuf_send(conn->proxy_iob, bdata(xff), blength(xff));
     check(rc > 0, "Failed to send to proxy.");
 
     rc = IOBuf_send(conn->proxy_iob, IOBuf_start(conn->iob)+req_len-2, cont_len+2);
@@ -56,6 +48,7 @@ StateEvent filter_transition(StateEvent state, Connection *conn, tns_value_t *co
 
 error:
     next = State_exec(&conn->state, REMOTE_CLOSE, (void *)conn);
+    return next;
 }   
        
 
