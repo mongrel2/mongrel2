6c1c45e4d848193653f7155cf32996840c227b60
Perry Kundert <perry@hardconsulting.com>
2012-04-24 12:50:15 -0600

    Add 0MQ 3.1.0 compatibility

--- src/zmq_compat.h
+++ src/zmq_compat.h
@@ -39,9 +39,11 @@
 #include <zmq.h>
 
 /*
- * Defines the 0MQ 3.1 API in terms of the 0MQ 2.X API, for backwards
- * compatibility.  Simply include "zmq_compat.h" after (or instead of) including
- * <zmq.h>.  Then, write all zmq_{send,recv,poll} calls using the 0MQ 3.1 API.
+ * Defines the 0MQ 3.1.1 API in terms of the 0MQ 3.1.0 and 2.X APIs, for
+ * backwards compatibility (0MQ 3.0 is not supported, because it broke wire
+ * protocol compatibility, among other things).  Simply include "zmq_compat.h"
+ * after (or instead of) including <zmq.h>.  Then, write all
+ * zmq_{send,recv,poll} calls using the 0MQ 3.1 API.
  *
  * However, in 0MQ 3.1, zmq_msg_{send,recv} return -1 on error, and a positive
  * value on success; 0MQ 2.X zmq_{send,recv} always returned 0 on success.  All
@@ -54,11 +56,17 @@
 #   define ZMQ_DONTWAIT     ZMQ_NOBLOCK
 #endif
 #if ZMQ_VERSION_MAJOR == 2
-#   define zmq_msg_send(msg, sock, opt) zmq_send(sock, msg, opt)
-#   define zmq_msg_recv(msg, sock, opt) zmq_recv(sock, msg, opt)
-#   define ZMQ_POLL_MSEC    1000        //  zmq_poll is usec
+#  define zmq_msg_send(msg, sock, opt) zmq_send(sock, msg, opt)
+#  define zmq_msg_recv(msg, sock, opt) zmq_recv(sock, msg, opt)
+#  define ZMQ_POLL_MSEC    1000        //  zmq_poll is usec
 #elif ZMQ_VERSION_MAJOR == 3
-#   define ZMQ_POLL_MSEC    1           //  zmq_poll is msec
+#  if ZMQ_VERSION_MINOR < 1
+#    error "0MQ 3.0 not supported"
+#  elif ZMQ_VERSION_MINOR == 1 && ZMQ_VERSION_PATCH == 0
+#     define zmq_msg_send(msg, sock, opt) zmq_sendmsg(sock, msg, opt)
+#     define zmq_msg_recv(msg, sock, opt) zmq_recvmsg(sock, msg, opt)
+#  endif
+#  define ZMQ_POLL_MSEC    1           //  zmq_poll is msec
 #endif
 
 #endif /* ! defined( _ZMQ_COMPAT_H ) */
