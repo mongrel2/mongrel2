649ecb0ed9110927ee2971c451eb9d5d3ec6d103
Henry <krakensden@krakensden.com>
2013-10-11 15:35:54 -0700

    Only zmq 3.1.0 (beta) was missing zmq_msg_send

--- src/zmq_compat.h
+++ src/zmq_compat.h
@@ -62,9 +62,11 @@
 #elif ZMQ_VERSION_MAJOR > 2
 #  if ZMQ_VERSION_MAJOR == 3 && ZMQ_VERSION_MINOR < 1
 #    error "0MQ 3.0 not supported"
-#  else
-#     define zmq_msg_send(msg, sock, opt) zmq_sendmsg(sock, msg, opt)
-#     define zmq_msg_recv(msg, sock, opt) zmq_recvmsg(sock, msg, opt)
+#  elif ZMQ_VERSION_MAJOR == 3 && ZMQ_VERSION_MINOR == 1 
+#    if ZMQ_VERSION_PATCH == 0
+#      define zmq_msg_send(msg, sock, opt) zmq_sendmsg(sock, msg, opt)
+#      define zmq_msg_recv(msg, sock, opt) zmq_recvmsg(sock, msg, opt)
+#    endif
 #  endif
 #  define ZMQ_POLL_MSEC    1           //  zmq_poll is msec
 #endif
