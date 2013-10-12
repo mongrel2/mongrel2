99592bf4c64cc5660f995eb78ac447fb30f2f25b
Jason Miller <jason@milr.com>
2013-10-11 22:52:49 -0700

    Fix zmq references
    Address PRs #193 and #194

--- docs/manual/inputs/install_dependencies.sh
+++ docs/manual/inputs/install_dependencies.sh
@@ -1,7 +1,7 @@
 # install ZeroMQ
-wget http://download.zeromq.org/zeromq-2.1.4.tar.gz
-tar -xzvf zeromq-2.1.4.tar.gz
-cd zeromq-2.1.4/
+wget http://download.zeromq.org/zeromq-2.1.7.tar.gz
+tar -xzvf zeromq-2.1.7.tar.gz
+cd zeromq-2.1.7/
 ./configure
 make
 sudo make install
