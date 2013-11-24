82ea4a5e255c6484f66ce245a2fec3e3fac57615
MinRK <benjaminrk@gmail.com>
2013-11-24 12:33:31 -0800

    add zmq_compat check for libzmq 4
    Fixes missing ZMQ_POLL_MSEC, which was undefined with libzmq 4.
    No other necessary changes.

--- src/zmq_compat.h
+++ src/zmq_compat.h
@@ -67,6 +67,8 @@
 #     define zmq_msg_recv(msg, sock, opt) zmq_recvmsg(sock, msg, opt)
 #  endif
 #  define ZMQ_POLL_MSEC    1           //  zmq_poll is msec
+#elif ZMQ_VERSION_MAJOR >= 4
+#  define ZMQ_POLL_MSEC    1           //  zmq_poll is msec
 #endif
 
 #endif /* ! defined( _ZMQ_COMPAT_H ) */
