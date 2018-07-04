c5116cb2da486115114daadf73a50b44bf971a11
Jason Miller <jason@milr.com>
2018-07-04 13:10:13 -0700

    Update release configuration

--- release-config.sh
+++ release-config.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 export REPO=mongrel2
-export NUMERIC_VERSION=1.11.0
+export NUMERIC_VERSION=1.12.0
 export OWNER=mongrel2
 export REPO=mongrel2
 export SITE_REPO=website
 export CHECKOUT_PATH="$(pwd)"
 export AUTH_FILE="${1:?Must specify curl authentication file}"
-#export REALLY_RELEASE=yes
+export REALLY_RELEASE=yes
 
 # Lines below here typically shouldn't need changing
 export RELEASE_NAME="v$NUMERIC_VERSION"
